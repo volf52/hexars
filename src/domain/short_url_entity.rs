bef998f5e89ee7db3143a49a81340857ba0f5086
Arslan <rslnkrmt2552@gmail.com>
2022-09-13 02:00:40 +0500

    add some tests

--- src/domain/short_url_entity.rs
+++ src/domain/short_url_entity.rs
@@ -1,5 +1,6 @@
 use super::base::{gen_id, BaseEntity};
 
+#[derive(Debug)]
 pub struct ShortUrl {
     id: String,
     url: String,
@@ -12,13 +13,61 @@ impl BaseEntity for ShortUrl {
 }
 
 impl ShortUrl {
-    pub fn new(url: String) -> Self {
+    pub fn new(url: String) -> Result<Self, ShortUrlError> {
         let id = gen_id();
 
-        Self { id, url }
+        ShortUrl::validate_url(&url)?;
+
+        Ok(Self { id, url })
     }
 
     pub fn url(&self) -> String {
         self.url.clone()
+    }
+
+    fn validate_url(url: &str) -> Result<(), ShortUrlError> {
+        url::Url::parse(url)
+            .map(|_| ())
+            .map_err(|e| ShortUrlError::InvalidUrl(url.to_string()))
+    }
+}
+
+// ---------- Exceptions --------
+#[derive(Debug, thiserror::Error, PartialEq)]
+pub enum ShortUrlError {
+    #[error("URL is not valid <{0}>")]
+    InvalidUrl(String),
+}
+
+#[cfg(test)]
+mod tests {
+    mod short_url {
+        use crate::domain::base::BaseEntity;
+        use crate::domain::short_url_entity::{ShortUrl, ShortUrlError};
+
+        #[test]
+        fn new_with_invalid_url() {
+            let url = "abc".to_string();
+
+            let n_result = ShortUrl::new(url.clone());
+
+            assert!(n_result.is_err());
+
+            assert_eq!(n_result.unwrap_err(), ShortUrlError::InvalidUrl(url));
+        }
+
+        #[test]
+        fn new_with_valid_url() {
+            let url = "https://www.google.com".to_string();
+
+            let result = ShortUrl::new(url.clone());
+
+            assert!(result.is_ok());
+
+            let ent = result.unwrap();
+
+            assert_eq!(ent.id().len(), 6);
+            assert_eq!(ent.url(), url);
+        }
     }
 }
