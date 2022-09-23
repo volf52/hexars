b5d1585c94d2749760197b4f414b9777fd6767ad
Arslan <rslnkrmt2552@gmail.com>
2022-09-23 20:24:38 +0500

    add tracing middleware

--- hexars/src/web/server.rs
+++ hexars/src/web/server.rs
@@ -1,8 +1,32 @@
 use hexars::{db::init_db, errors::LoginError, infra::di};
+use poem::{
+    get, handler, listener::TcpListener, middleware::Tracing, web::Json, EndpointExt, Route, Server,
+};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct HealthStatus {
+    status: String,
+}
+
+impl HealthStatus {
+    pub fn ok() -> Self {
+        Self {
+            status: "ok".to_string(),
+        }
+    }
+}
+
+#[handler]
+async fn health() -> Json<HealthStatus> {
+    Json(HealthStatus::ok())
+}
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
+    std::env::set_var("RUST_LOG", "poem=debug");
+    tracing_subscriber::fmt::init();
 
     dotenv::dotenv().ok();
 
@@ -11,16 +35,22 @@ async fn main() -> color_eyre::Result<()> {
     let container = di::Container::init();
     let serv = &container.short_url;
 
-    let e = serv
-        .create_short_url("https://www.facebook.com".to_string())
-        .await
-        .map_err(|_| LoginError::MissingDetails)?;
+    // let e = serv
+    //     .create_short_url("https://www.facebook.com".to_string())
+    //     .await
+    //     .map_err(|_| LoginError::MissingDetails)?;
+    //
+    // println!("Inserted: {:#?}", e);
+    //
+    // let ents = &container.short_url.get_all_urls().await;
+    //
+    // println!("All: {:#?}", ents);
 
-    println!("Inserted: {:#?}", e);
+    let app = Route::new().at("/health", get(health)).with(Tracing);
 
-    let ents = &container.short_url.get_all_urls().await;
-
-    println!("All: {:#?}", ents);
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+        .run(app)
+        .await?;
 
     Ok(())
 }
